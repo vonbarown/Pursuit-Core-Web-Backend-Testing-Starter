4d73230bfe4ea995209eba58341e8ea621e03558
alejo4373 <alejandrofranco@pursuit.org>
2020-05-02 04:22:06 -0400

    remove tests and setup

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-// Set test database DATABASE_URL for testing so that our development database 
-// does not get reset in between test
-process.env.DATABASE_URL = "postgres://localhost:5432/users_notes_api_test_db"
-
-module.exports = {
-  setupFilesAfterEnv: ["jest-extended"],
-  testEnvironment: "node"
-}
