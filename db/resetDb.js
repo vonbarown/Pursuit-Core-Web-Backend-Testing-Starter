efaaae0bc50162b4c367aaa2f39e3dcce621c402
alejo4373 <alejandrofranco@pursuit.org>
2020-05-02 04:18:22 -0400

    add explantion to resetDb.js

--- db/resetDb.js
+++ db/resetDb.js
@@ -1,6 +1,6 @@
 /*
  * resetDb.js
- * File used to reset and reseed database tables for testing purposes
+ * File used to reset and reseed database tables when testing 
  */
 
 const { execSync } = require('child_process')
@@ -8,6 +8,7 @@ const { execSync } = require('child_process')
 const SEED_FILE_PATH = __dirname + "/seed.sql"
 
 // Having into account that DATABASE_URL = "postgres://localhost:5432/users_notes_api_test_db"
+// was set in jest.config.js
 const DATABASE_NAME = process.env.DATABASE_URL.split('/')[3]
 
 const resetDb = () => {
